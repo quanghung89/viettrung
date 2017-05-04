ccbedc14db8f2ed9ec4bcb2d639bd3cb9067eb3f
Truong Quang Hung <hung.truongquang@imipgroup.com>
2017-05-04 23:36:16 +0700

    Commit source code

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/order/admin/');
+define('HTTP_CATALOG', 'http://localhost/order/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://localhost/order/admin/');
+define('HTTPS_CATALOG', 'https://localhost/order/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/order/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/order/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/order/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/order/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/order/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/order/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/order/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/order/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/order/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/order/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/order/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/order/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'viettrung');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
